03df0d7e6d9246600b00160461a83e1e32d92936
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-04 16:31:15 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,6 +6,21 @@ class PigLatinizer
   end
 
   def piglatinize_word(word)
+    letter = word[0].downcase # "word" 
+    if letter.start_with?(/[aeiou]/) # if "word" doesn't start with a vowel, "wordway"
+      "#{word}way"
+    else
+      constonants = [] # save constonant to add from letters array 
+      # iterate through array of letters, adding to constanants if not a vowel
+      letters.each do |letter|
+        until letter.start_with?(/[aeiou]/)
+          constonants << letter
+        end
+        binding.pry 
+      end 
+    end 
+  end 
+    
 
   end
 
@@ -16,3 +31,4 @@ class PigLatinizer
   end
 
 end
+
